901433270e5dc6d1360e0fbf9e3c0e85c7392611
Dankrushen <henryclose@gmail.com>
2018-06-21 14:51:15 -0400

    Initial project commit

---
+++ OpenPose-CSharp-Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPose_CSharp_Demo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			OpenPose_Reader reader = new OpenPose_Reader();
+		}
+	}
+}
