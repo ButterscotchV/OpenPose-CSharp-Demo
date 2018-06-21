315a2e59c776349e024240c71207d3fc4d1bafc7
Dankrushen <henryclose@gmail.com>
2018-06-21 18:21:59 -0400

    Fix reference, run with simple static dir

--- OpenPose-CSharp-Demo/Program.cs
+++ OpenPose-CSharp-Demo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenPose;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,28 @@ using System.Threading.Tasks;
 
 namespace OpenPose_CSharp_Demo
 {
-	class Program
+	public class Program
 	{
+		public static PoseEventListener listener = new PoseEventListener();
+
 		static void Main(string[] args)
 		{
-			OpenPose_Reader reader = new OpenPose_Reader();
+			OpenPose_Reader reader = new OpenPose_Reader("C:\\Users\\Dankrushen\\openpose-1.3.0-win64-gpu-binaries\\JSON_Output\\", listener);
+
+			reader.AsynchronousQueueReader().Join();
+		}
+	}
+
+	public class PoseEventListener : IPoseEvent
+	{
+		public void OnPoseGenerated(Pose2D pose)
+		{
+			//Console.WriteLine("Test");
+
+			foreach (KeyPoint2D keypoint in pose.KeyPoints)
+			{
+				Console.WriteLine(keypoint.ToString());
+			}
 		}
 	}
 }
