32e1e86c01aef0f4abf2aaa8f88218e2e23db317
Dankrushen <henryclose@gmail.com>
2018-06-22 20:29:10 -0400

    Implement VRidge API & First working version

---
+++ OpenPose-CSharp-Demo/Helpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Media.Media3D;
+
+namespace OpenPose_CSharp_Demo
+{
+	public static class Helpers
+	{
+		public static Quaternion QuaternionFromYawPitchRoll(float yaw, float pitch, float roll)
+		{
+			double angleSquared = pitch * pitch + yaw * yaw + roll * roll;
+			double s = 0;
+			double c = 1;
+			if (angleSquared > 0)
+			{
+				double angle = Math.Sqrt(angleSquared);
+				s = Math.Sin(angle * 0.5f) / angle;
+				c = Math.Cos(angle * 0.5f);
+			}
+
+			return new Quaternion(s * pitch, s * yaw, s * roll, c);
+		}
+
+		public static float[] DoubleArraytoFloatArray(double[] array)
+		{
+			float[] newArray = new float[array.Length];
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				newArray[i] = (float)array[i];
+			}
+
+			return newArray;
+		}
+	}
+}
--- OpenPose-CSharp-Demo/Program.cs
+++ OpenPose-CSharp-Demo/Program.cs
@@ -1,33 +1,159 @@
 ﻿using OpenPose;
+using OpenPose.Pose;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using VRE.Vridge.API.Client.Messages.Control;
+using VRE.Vridge.API.Client.Messages.v3.Broadcast;
+using VRE.Vridge.API.Client.Proxy;
+using VRE.Vridge.API.Client.Proxy.Broadcasts;
+using VRE.Vridge.API.Client.Proxy.Controller;
+using VRE.Vridge.API.Client.Proxy.HeadTracking;
 
 namespace OpenPose_CSharp_Demo
 {
 	public class Program
 	{
-		public static PoseEventListener listener = new PoseEventListener();
+		// Haptic pulse info
+		private DateTime? lastHapticPulseTime = null;
+		private uint lastHapticPulseLengthUs = 0;
+
+		// API client         
+		private readonly APIClient apiClient = new APIClient("OpenPose CSharp Demo");
+
+		// Helper services
+		public TrackingService headTrackingService;
+		public ControllerService controllerService;
+		public ControllerService controllerService2;
+
+		// Broadcast listener
+		private BroadcastProxy broadcastProxy;
+
+		public static PoseEventListener listener;
 
 		static void Main(string[] args)
 		{
-			OpenPose_Reader reader = new OpenPose_Reader("C:\\Users\\Dankrushen\\openpose-1.3.0-win64-gpu-binaries\\JSON_Output\\", listener);
+			Program program = new Program();
+		}
+
+		public Program()
+		{
+			// Try connecting by default
+			ConnectOrReconnect();
+
+			listener = new PoseEventListener(this);
+			KeyPoint2D.X_Scale_PPM = 426.6f;
+			KeyPoint2D.Y_Scale_PPM = 447.2f;
+
+			OpenPose_Reader reader = new OpenPose_Reader2D("C:\\Users\\Dankrushen\\openpose-1.3.0-win64-gpu-binaries\\JSON_Output\\", listener);
 
 			reader.AsynchronousQueueReader().Join();
+		}
+
+		private void ConnectOrReconnect()
+		{
+			try
+			{
+				// Connect to the services
+				headTrackingService = new TrackingService(apiClient.CreateProxy<HeadTrackingProxy>());
+				controllerService = new ControllerService(apiClient.CreateProxy<ControllerProxy>());
+				controllerService2 = new ControllerService(apiClient.CreateProxy<ControllerProxy>());
+
+				broadcastProxy = apiClient.CreateProxy<BroadcastProxy>();
+				broadcastProxy.HapticPulseReceived += OnHapticFeedbackReceived;
+
+				headTrackingService.ChangeStatus(true);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+		}
+
+		private string CheckStatus()
+		{
+			var status = apiClient.GetStatus();
+			if (status == null)
+			{
+				return null;
+			}
+
+			var msg = "API endpoint status: ";
+			status.Endpoints.ForEach(e => msg += $"{e.Name} is {(ControlResponseCode)e.Code}, ");
+			return msg;
+		}
+
+		private void ResetAsyncRotation()
+		{
+			headTrackingService.ResetAsyncOffset();
+		}
+
+		private void RecenterView()
+		{
+			headTrackingService.RecenterView();
+		}
+
+		private void OnHapticFeedbackReceived(object sender, HapticPulse hapticPulse)
+		{
+			lastHapticPulseLengthUs = hapticPulse.LengthUs;
+			lastHapticPulseTime = DateTime.Now;
 		}
 	}
 
 	public class PoseEventListener : IPoseEvent
 	{
-		public void OnPoseGenerated(Pose2D pose)
+		private Program program;
+
+		public PoseEventListener(Program program)
+		{
+			this.program = program;
+		}
+
+		public void OnPoseGenerated(Pose pose)
 		{
 			//Console.WriteLine("Test");
 
-			foreach (KeyPoint2D keypoint in pose.KeyPoints)
+			if (pose != null && typeof(Pose2D) == pose.GetType())
 			{
-				Console.WriteLine(keypoint.ToString());
+				Pose2D pose2D = (Pose2D)pose;
+
+				KeyPoint2D nose = pose2D.GetKeyPoint2D(BodyPoint.Nose);
+				//KeyPoint2D leftEye = pose2D.GetKeyPoint2D(BodyPoint.Left_Eye);
+				//KeyPoint2D rightEye = pose2D.GetKeyPoint2D(BodyPoint.Right_Eye);
+
+				if (nose != null && nose.IsValid)//&& leftEye != null && rightEye != null && nose.IsValid && leftEye.IsValid && rightEye.IsValid)
+				{
+					program.headTrackingService.ChangeStatus(true);
+
+					//float height = (leftEye.Y + rightEye.Y) / 2;
+					program.headTrackingService.SendPositionOnly(nose.X, nose.Y, 0);
+				}
+				else
+				{
+					program.headTrackingService.ChangeStatus(false);
+				}
+
+				KeyPoint2D rightHand = pose2D.GetKeyPoint2D(BodyPoint.Right_Wrist);
+
+				if (rightHand != null && rightHand.IsValid)
+				{
+					program.controllerService.SetControllerState(0, rightHand.X, rightHand.Y, -0.5, 0, 0, 0, 0, 0, 0, false, false, false);
+				}
+
+				KeyPoint2D leftHand = pose2D.GetKeyPoint2D(BodyPoint.Left_Wrist);
+
+				if (leftHand != null && leftHand.IsValid)
+				{
+					program.controllerService2.SetControllerState(1, leftHand.X, leftHand.Y, -0.5, 0, 0, 0, 0, 0, 0, false, false, false);
+				}
+			}
+			else
+			{
+				throw (new Exception("Not Pose2D."));
 			}
 		}
 	}
